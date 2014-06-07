eb324250d3c1252b9d4d9ec1438d824edc379874
kimsavinfo <kimsavinfo@gmail.com>
2014-06-07 19:34:20 +0200

    classe de test
    UserTest qui hérite de JpaTestCase

---
+++ src/test/java/fr/epsi/JpaUserTest.java
@@ -0,0 +1,36 @@
+package fr.epsi;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class JpaUserTest extends JpaTestCase
+{
+	private static EntityManagerFactory entityManagerFactory;
+	protected EntityManager entityManager;
+
+	@BeforeClass
+	public static void createEntityManagerFactory() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("myPersistenceUnit");
+	}
+
+	@AfterClass
+	public static void closeEntityManagerFactory() {
+		entityManagerFactory.close();
+	}
+
+	@Before
+	public void createEntityManager() {
+		entityManager = entityManagerFactory.createEntityManager();
+	}
+
+	@After
+	public void closeEntityManager() {
+		entityManager.close();
+	}
+}
