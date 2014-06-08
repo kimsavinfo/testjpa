6b2f983b4aef3df945522fcbb87896f99f7cec7d
kimsavinfo <kimsavinfo@gmail.com>
2014-06-08 11:18:02 +0200

    entity manager probl
    null pointer exception

--- src/main/java/fr/epsi/Event.java
+++ src/main/java/fr/epsi/Event.java
@@ -2,20 +2,13 @@ package fr.epsi;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.List;
 import java.util.SimpleTimeZone;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
---
+++ src/test/java/fr/epsi/EventTest.java
@@ -0,0 +1,24 @@
+package fr.epsi;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EventTest extends JpaTestCase
+{
+	
+	@Test
+	public void test()
+	{		
+		assertEquals("ok", "ok");
+	}
+
+}
