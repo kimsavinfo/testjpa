a5c49e68a50929ba6eca412928b7928c59862ecd
kimsavinfo <kimsavinfo@gmail.com>
2014-06-09 22:06:53 +0200

    Johan solution
    oki

--- src/main/java/fr/epsi/Event.java
+++ src/main/java/fr/epsi/Event.java
@@ -1,68 +1,79 @@
 package fr.epsi;
 
-import java.io.Serializable;
 import java.util.Calendar;
-import javax.persistence.*;
+import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
 
-/**
- * Entity implementation class for Entity: Event
- *
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 @Entity
-@Table(name="events_01")
-public class Event implements Serializable {
-	private static final long serialVersionUID = 1L;
+@Table(name="events")
+public class Event {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="events_id")
+	private Integer id;
+	@Column(name="events_title", nullable=false)
+	private String title;
+	@Column(name="events_description")
+	private String description;
+	@Column(name="events_beginDate")
+	@Temporal(TemporalType.DATE)
+	private Calendar beginDate;
+	@Column(name="events_allDay")
+	private boolean allDay;
 	
-		@Id
-		@GeneratedValue(strategy=GenerationType.AUTO)
-		@Column(name="events_id")
-		private Integer id;
-		@Column(name="events_title", nullable=false)
-		private String title;
-		@Column(name="events_description")
-		private String description;
-		@Column(name="events_beginDate")
-		@Temporal(TemporalType.DATE)
-		private Calendar beginDate;
-		@Column(name="events_allDay")
-		private boolean allDay;
+	public Event() {
+	}
 
+	public Event (String title, String description, boolean allDay) {
+		this.title = title;
+		this.description = description;
+		this.allDay = allDay;
 
-		public Event() {
-			super();
-		}
-		// Getters et setters
+		this.beginDate = new GregorianCalendar(new SimpleTimeZone(3600000, "Europe/Paris",
+				Calendar.MARCH, -1, Calendar.SUNDAY, 3600000, SimpleTimeZone.UTC_TIME,
+				Calendar.OCTOBER, -1, Calendar.SUNDAY, 3600000, SimpleTimeZone.UTC_TIME,
+				3600000));
+	}
 
-		public Integer getId() {
-			return id;
-		}
-		public void setId(Integer id) {
-			this.id = id;
-		}
-		public String getTitle() {
-			return title;
-		}
-		public void setTitle(String title) {
-			this.title = title;
-		}
-		public String getDescription() {
-			return description;
-		}
-		public void setDescription(String description) {
-			this.description = description;
-		}
-		public Calendar getBeginDate() {
-			return beginDate;
-		}
-		public void setBeginDate(Calendar beginDate) {
-			this.beginDate = beginDate;
-		}
-		public boolean isAllDay() {
-			return allDay;
-		}
-		public void setAllDay(boolean allDay) {
-			this.allDay = allDay;
-		}
+	// Getters et setters
 
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Calendar getBeginDate() {
+		return beginDate;
+	}
+	public void setBeginDate(Calendar beginDate) {
+		this.beginDate = beginDate;
+	}
+	public boolean isAllDay() {
+		return allDay;
+	}
+	public void setAllDay(boolean allDay) {
+		this.allDay = allDay;
+	}
 }
-
--- src/test/java/fr/epsi/EventTest.java
+++ src/test/java/fr/epsi/EventTest.java
@@ -2,122 +2,30 @@ package fr.epsi;
 
 import static org.junit.Assert.*;
 
-import java.util.GregorianCalendar;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class EventTest extends JpaTestCase 
+public class EventTest extends JpaTestCase
 {
-	private static EntityManagerFactory entityManagerFactory;
-
-	@BeforeClass
-	public static void createEntityManagerFactory() 
-	{
-		entityManagerFactory = Persistence.createEntityManagerFactory("EpsiPersistenceUnit");
-	}
-
 	
 	@Test
-	public void createEvent()
-	{
-		
-	/*	entityManagerFactory = Persistence.createEntityManagerFactory("EpsiPersistenceUnit");
-		entityManager = entityManagerFactory.createEntityManager();
-		
-		Event event = new Event();
-		event.setTitle("Partiels");
-		event.setDescription("A vos révisions je vous prie.");
-		event.setBeginDate( new GregorianCalendar(2014,07,14) );
-		event.setAllDay(false);
-		
-		
-		entityManager.getTransaction().begin();
-		boolean transactionOk = false;
-		try {
-			entityManager.persist(event);
-			
-			transactionOk = true;
-		}
-		finally {
-			if(transactionOk) {
-				entityManager.getTransaction().commit();
-				
-				Event eventSaved = entityManager.find(Event.class, 1);
-				assertEquals(event.getTitle(), eventSaved.getTitle());
-				assertEquals(event.getDescription(), eventSaved.getDescription());
-				assertEquals(event.getBeginDate(), eventSaved.getBeginDate());
-				assertEquals(event.isAllDay(), eventSaved.isAllDay());
-				
-				entityManager.close();
-				entityManagerFactory.close();
-			}
-			else {
-				entityManager.getTransaction().rollback();
-			}
-		}
-		*/
+	public void test()
+	{		
+		assertEquals("ok", "ok");
 	}
 	
 
-
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		// Création EntityManagerFactory
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("EpsiPersistenceUnit"); 
-
-		// ********************** Insertion de 2 evenements dans la table		
-		// Création EntityManager et Transaction		
-		EntityManager em = emf.createEntityManager();   
-		EntityTransaction transac = em.getTransaction();
-		transac.begin();
-		//	    Création évenement 1
-		Event e1 = new Event();
-		e1.setTitle("Titre de l'event 1");
-		e1.setDescription("Description de l'évènement 1");
-		e1.setBeginDate(new GregorianCalendar());
-		e1.setAllDay(false);
-		//		 Création évenement 2
-		Event e2 = new Event();
-		e2.setTitle("Titre de l'event 2");
-		e2.setDescription("Description de l'évènement 2");
-		e2.setBeginDate(new GregorianCalendar());
-		e2.setAllDay(true);
-		// Stockage des évenements dans la table
-		em.persist(e1);
-		em.persist(e2);
-		transac.commit();
-		// Fermeture EntityManager	       
-		em.close(); 
-
-		// ************ Lecture dans la table
-
-		EntityManager em2 = emf.createEntityManager();   
-		EntityTransaction transac2 = em2.getTransaction();
-		transac2.begin();
-		// Récupération de l'evenement d'Id = 2 et affichage dans la console pour vérification
-		Event ev = em2.find(Event.class, 2);
-		System.out.println("Event : [id] = " + ev.getId() + "\t" +
-				"[title] = " + ev.getTitle() + "\t" +
-				"[desc] = " + ev.getDescription() + "\t" + 
-				"[date] = " + ev.getBeginDate().getTime().toLocaleString() + "\t" + 
-				"[allDay] = " + ev.isAllDay());
-
-		transac2.commit();
-		em2.close(); 
-
-		emf.close();
-
+	@Test
+	public void enregistrerEntite()
+	{		
+		assertEquals("ok", "ok");
 	}
-	
-	
 
 }
