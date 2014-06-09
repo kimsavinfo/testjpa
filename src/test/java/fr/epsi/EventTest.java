b6d835e0f61a3ac085792a4106669a046bbcbf1b
kimsavinfo <kimsavinfo@gmail.com>
2014-06-09 22:21:47 +0200

    CRU

--- src/test/java/fr/epsi/EventTest.java
+++ src/test/java/fr/epsi/EventTest.java
@@ -2,6 +2,8 @@ package fr.epsi;
 
 import static org.junit.Assert.*;
 
+import java.util.GregorianCalendar;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,18 +16,64 @@ import org.junit.Test;
 
 public class EventTest extends JpaTestCase
 {
-	
 	@Test
-	public void test()
+	public void testCreate()
 	{		
-		assertEquals("ok", "ok");
+		Event event = new Event();
+		event.setTitle("Partiels");
+		event.setDescription("A vos révisions je vous prie.");
+		event.setBeginDate( new GregorianCalendar(2014,07,14) );
+		event.setAllDay(false);
+		
+		
+		entityManager.getTransaction().begin();
+		boolean transactionOk = false;
+		try {
+			entityManager.persist(event);
+			
+			transactionOk = true;
+		}
+		finally {
+			if(transactionOk) {
+				entityManager.getTransaction().commit();
+				
+				Event eventSaved = entityManager.find(Event.class, 1);
+				assertEquals(event.getTitle(), eventSaved.getTitle());
+				assertEquals(event.getDescription(), eventSaved.getDescription());
+				assertEquals(event.getBeginDate(), eventSaved.getBeginDate());
+				assertEquals(event.isAllDay(), eventSaved.isAllDay());
+			}
+			else {
+				entityManager.getTransaction().rollback();
+			}
+		}
 	}
 	
-
 	@Test
-	public void enregistrerEntite()
+	public void testUpdate()
 	{		
-		assertEquals("ok", "ok");
+		String title = "Le nouveau titre";
+		Event event = entityManager.find(Event.class, 1);
+		event.setTitle(title);
+		
+		entityManager.getTransaction().begin();
+		boolean transactionOk = false;
+		try {
+			entityManager.persist(event);
+			
+			transactionOk = true;
+		}
+		finally {
+			if(transactionOk) {
+				entityManager.getTransaction().commit();
+				
+				Event eventSaved = entityManager.find(Event.class, 1);
+				assertEquals(title, eventSaved.getTitle());
+			}
+			else {
+				entityManager.getTransaction().rollback();
+			}
+		}
 	}
 
 }
